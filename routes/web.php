eeab65c1750f28fc96d510536c106f225fa914b8
MohammadJavad <mj.ranjbar.94@gmail.com>
2019-05-16 08:36:01 -0400

    Add Threats

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-	return view('welcome');
-});
+Route::get('/','ThreadsController@index');
 
 
 Route::get('/threads', 'ThreadsController@index')->name('threads');
